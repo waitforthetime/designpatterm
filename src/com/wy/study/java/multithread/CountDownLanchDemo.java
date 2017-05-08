7bb39f7f3f0f3e1ea296b262205cbc3ba5de9639
wangyuan <313799398@qq.com>
2017-05-08 23:58:08 +0800

    CountDownLanch的测试

---
+++ src/com/wy/study/java/multithread/CountDownLanchDemo.java
@@ -0,0 +1,87 @@
+package com.wy.study.java.multithread;
+
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Created by wy_ms on 2017/05/08.
+ */
+public class CountDownLanchDemo {
+
+    public static void main(String[] args) {
+
+//        normalTest(10);
+        Mytest(10);
+
+    }
+
+    public static void Mytest(int count) {
+        try {
+            CountDownLatch startCount = new CountDownLatch(1);
+            CountDownLatch endCount = new CountDownLatch(count);
+            for (int i = 0; i < count+1; i++) {
+                new Thread(new CountDownRun(startCount, endCount, "" + i)).start();
+            }
+            System.out.println("线程的小伙伴还没开始呢，都在等我呢,"+System.currentTimeMillis());
+            Thread.sleep(1000);
+            System.out.println("该让小伙伴干事了，"+System.currentTimeMillis());
+            startCount.countDown();
+            System.out.println("我要等小伙伴都干完了，我在干,"+System.currentTimeMillis());
+            endCount.await();
+            System.out.println("我要开始干事了，"+System.currentTimeMillis());
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void normalTest(int count) {
+        try {
+            CountDownLatch startCount = new CountDownLatch(1);
+            CountDownLatch endCount = new CountDownLatch(count);
+            for (int i = 0; i < count; i++) {
+                new Thread(new CountDownRun(startCount, endCount, "" + i)).start();
+            }
+            System.out.println("线程的小伙伴还没开始呢，都在等我呢,"+System.currentTimeMillis());
+            Thread.sleep(1000);
+            System.out.println("该让小伙伴干事了，"+System.currentTimeMillis());
+            startCount.countDown();
+            System.out.println("我要等小伙伴都干完了，我在干,"+System.currentTimeMillis());
+            endCount.await();
+            System.out.println("我要开始干事了，"+System.currentTimeMillis());
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    static class CountDownRun implements Runnable {
+
+        CountDownLatch startCount;
+        CountDownLatch endCount;
+        String name;
+
+        public CountDownRun(CountDownLatch startCount, CountDownLatch endCount, String name) {
+            this.startCount = startCount;
+            this.endCount = endCount;
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+
+            try {
+                startCount.await();
+                System.out.println("线程：" + name + "开始");
+                System.out.println("线程："+name+"开始do something");
+                int rand = new Random().nextInt(20);
+                Thread.sleep(rand*1000);
+                System.out.println("线程："+name+"is over");
+                endCount.countDown();
+            } catch (Throwable e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/com/wy/study/java/multithread/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by wy_ms on 2017/05/07.
+ */
+package com.wy.study.java.multithread;
